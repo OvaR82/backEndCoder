33765eb2f3a61bd6bb3f9dbfedd161fd09fb92e2
Ova.R <69318757+OvaR82@users.noreply.github.com>
2022-03-26 21:05:45 -0300

    Revisión

--- WebSockets/src/principal.js
+++ WebSockets/src/principal.js
@@ -1,19 +1,13 @@
 /* -------------------------------------------------------------------------- */
-/*                     5to Desafío: Motores de Plantillas                     */
+/*                     6to Desafío: WebSockets                                */
 /* -------------------------------------------------------------------------- */
 
 /* -------------------------------- Consigna: ------------------------------- */
 /*
-1) Utilizando la misma API de productos del proyecto entregable de la clase anterior, construir un web server (no REST) que incorpore:
-    Un formulario de carga de productos en la ruta raíz (configurar la ruta '/productos' para recibir el POST, y redirigir al mismo formulario).
-    Una vista de los productos cargados (utilizando plantillas de handlebars) en la ruta GET '/productos'.
-    Ambas páginas contarán con un botón que redirija a la otra.
-
-2) Manteniendo la misma funcionalidad reemplazar el motor de plantillas handlebars por pug.
-
-3) Manteniendo la misma funcionalidad reemplazar el motor de plantillas handlebars por ejs.
-
-4) Por escrito, indicar cuál de los tres motores de plantillas prefieres para tu proyecto y por qué.
+Consigna 1:  Modificar el último entregable para que disponga de un canal de websocket que permita representar, 
+    por debajo del formulario de ingreso, una tabla con la lista de productos en tiempo real. 
+    Puede haber varios clientes conectados simultáneamente y en cada uno de ellos se reflejarán los cambios que se realicen en los productos sin necesidad de recargar la vista.
+    Cuando un cliente se conecte, recibirá la lista de productos a representar en la vista.
 */
 
 const express = require('express')
